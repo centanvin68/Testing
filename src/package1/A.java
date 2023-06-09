eae02ccce22bd412d519d72c6eda303a668c93f2
johng <johng@DESKTOP-4SJACL6.mshome.net>
2023-06-08 23:24:08 -0400

    DAY 3

---
+++ src/package1/A.java
@@ -0,0 +1,22 @@
+package package1;
+
+public class A {
+
+	//public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	}
+class B
+{
+
+}
+class C
+{
+	
+}
+class D
+{
+	
+}
+
---
+++ src/package1/Firstpgm.java
@@ -0,0 +1,15 @@
+package package1;
+
+public class Firstpgm {
+
+	//main ctrl+space
+	public static void main(String[] args) {
+		
+	//public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//sysout ctrl+space
+  System.out.println("Hello World");
+	}
+
+}
