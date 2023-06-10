c1a87ccf400f414a5402ffd983833955767397f2
johng <johng@DESKTOP-4SJACL6.mshome.net>
2023-06-10 00:31:35 -0400

    co

--- src/package1/A.java
+++ src/package1/A.java
@@ -2,10 +2,10 @@ package package1;
 
 public class A {
 
-	//public static void main(String[] args) {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		
+	
+	System.out.println(" N number of class but only 1 public file same as file name");
 	}
 class B
 {
@@ -18,5 +18,6 @@ class C
 class D
 {
 	
+}
 }
 
