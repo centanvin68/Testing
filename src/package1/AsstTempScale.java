e85a9f479f03708bc0f0b3818f1117dae06c25cc
johng <johng@host.docker.internal>
2023-06-17 16:38:26 -0400

    Assignment convert temperature to kelvin and fahrenheit

---
+++ src/package1/AsstTempScale.java
@@ -0,0 +1,23 @@
+package package1;
+
+public class AsstTempScale {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+     double c =0.0;
+     System.out.println("Input: " +c);
+     //System.out.println("convert celsius into Fahrenheit");
+     System.out.println();
+     double F = ((1.8*c)+32);
+     System.out.println("Output: ");
+     System.out.println(c +" Celsius= " +F +" Fahrenheit");
+     
+    
+     //System.out.println("convert celsius into kelvin");
+     double k = (c+273);
+     System.out.println(c +" Celsius= " +k +" Kelvin");
+		
+	}
+
+}
