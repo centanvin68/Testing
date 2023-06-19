578367ae8932911aeb6da69d45fd00b1f02908ff
johng <johng@DESKTOP-4SJACL6.mshome.net>
2023-06-18 21:02:22 -0400

    this is push assignment from local to remote

---
+++ src/package1/AsstPush.java
@@ -0,0 +1,10 @@
+package package1;
+
+public class AsstPush {
+     
+public static void main(String[] args) {
+	
+	System.out.println(" this is push assignament from eclipse to remote repository");
+}
+
+}
