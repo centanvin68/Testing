8f7ee8e33f325091c30962d476475f8e46b39e24
johng <johng@DESKTOP-4SJACL6>
2023-06-21 19:16:20 -0400

    Assignment infinite loops

---
+++ src/package1/InfiniteLoop.java
@@ -0,0 +1,15 @@
+package package1;
+
+public class InfiniteLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for( ;  ;  )
+		{
+			System.out.println("for loop infinite");
+		}
+		
+	}
+
+}
---
+++ src/package1/WhileInfiniteLoop.java
@@ -0,0 +1,15 @@
+package package1;
+
+public class WhileInfiniteLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;
+		while(i!=0)
+		{
+			System.out.println("while loop infinite");
+			i++;
+		}
+	}
+
+}
