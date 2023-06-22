636e7d13e545eb384f6a5cb0380c881e4b016df3
johng <johng@DESKTOP-4SJACL6>
2023-06-22 19:47:40 -0400

    assignment sum of digits

---
+++ src/package1/SumOfDigit.java
@@ -0,0 +1,24 @@
+package package1;
+
+public class SumOfDigit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=234, sum=0;
+		
+		while(a>0)
+			{
+		int b = a%10;
+		sum= sum+b;
+		a=a/10;
+		   }
+		
+		System.out.println("sum should be "+sum);
+		
+	}
+
+}
+//suppose value a = 234
+//sum = 2+3+4 = 9
+//sum should be 9
