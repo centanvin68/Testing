dfa7b17dcc3b45cdd3ebbb5e446f1a6b7f69f407
johng <johng@host.docker.internal>
2023-06-25 23:40:33 -0400

    assignment while infinite loop 2 method

---
+++ src/package1/AsstWhile2InfiniteLoop.java
@@ -0,0 +1,21 @@
+package package1;
+
+public class AsstWhile2InfiniteLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=1;
+		while(i!=0)
+		{
+			System.out.println("while loop  1st infinite method");
+			i++;
+		}
+		int j=1;
+		while(j>0)
+		{
+			System.out.println("2nd infinite method");
+			j++;
+		}
+	}
+
+}
