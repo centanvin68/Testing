33efde87e9160cd48ad906726e862425abde39f0
johng <johng@DESKTOP-4SJACL6>
2023-07-10 23:06:13 -0400

    error

---
+++ src/package1/AsstStarPattern1.java
@@ -0,0 +1,59 @@
+package package1;
+
+public class AsstStarPattern1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Pattern1");
+		for(int i=1; i<=5;i++)
+		{
+			for(int j=0;j<i;j++)
+			{
+			System.out.print("*");
+		    }
+			System.out.println();
+		}
+		
+		System.out.println("Pattern2");
+		for(int i=1; i<=5;i++)
+		{
+			for(int j=5;j<=5;j--)
+			{
+			System.out.print(" ");
+			}
+			System.out.println("*");
+		}
+		
+//		
+//		System.out.println("Pattern3");
+//		for(int i=1; i<=5;i++)
+//		{
+//			for(int j=0;j<i;j++)
+//			{
+//			System.out.print("*");
+//		    }
+//			System.out.println();
+//		}
+//		System.out.println("Pattern4");
+//		for(int i=1; i<=5;i++)
+//		{
+//			for(int j=0;j<i;j++)
+//			{
+//			System.out.print("*");
+//		    }
+//			System.out.println();
+//		}
+//		
+//		System.out.println("Pattern5");
+//		for(int i=1; i<=5;i++)
+//		{
+//			for(int j=0;j<i;j++)
+//			{
+//			System.out.print("*");
+//		    }
+//			System.out.println();
+//		}
+	}
+
+}
