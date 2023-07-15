698e00114d5d065bc20e2bf83fc9615170baa0d4
johng <johng@DESKTOP-4SJACL6.ht.home>
2023-07-14 22:50:42 -0400

    For loop 2 infinite methods

---
+++ src/package1/AsstLoop.java
@@ -0,0 +1,19 @@
+package package1;
+
+public class AsstLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+
+//			    for(;;) 
+//			    {
+//			    System.out.println("Hello 1st method"); 
+//			    } 
+			    for(int i=1;i>0;i++)
+			    {
+			    	System.out.println("hello 2nd method");
+			    }
+
+	}
+
+}
